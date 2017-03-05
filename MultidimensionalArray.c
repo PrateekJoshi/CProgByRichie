ddb8a26b47ab57c7fae52b8d3ae5fa951f8f2484
prateek <dell pc@dell>
2017-03-05 12:54:00 +0530

    Added linked list

--- MultidimensionalArray.c
+++ MultidimensionalArray.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-void display(int(*)[5],int);
+void display(int (*)[5], int);
+void display2(int[][5], int);
+void display3(int *, int, int);
+
 int main(int argc, char **argv) {
 	int arr[5][5];
 	for (int i = 0; i < 5; i++) {
-		for(int j=0;j<5;j++){
-			arr[i][j]=5;
+		for (int j = 0; j < 5; j++) {
+			arr[i][j] = 5;
 		}
 	}
-	display(arr,5);
+	printf("Method 1:\n");
+	display(arr, 5);
+	printf("Method 2:\n");
+	display2(arr, 5);
+	printf("Method 3:\n");
+	display3(&arr[0][0], 5, 5);
 	return 0;
 }
 
-void display(int (*arr)[5],int size){
+void display(int (*arr)[5], int size) {
 	for (int i = 0; i < 5; i++) {
-			for(int j=0;j<5;j++){
-				printf("%d ",arr[i][j]);
-			}
-			printf("\n");
+		for (int j = 0; j < 5; j++) {
+			printf("%d ", arr[i][j]);
 		}
+		printf("\n");
+	}
 }
 
+void display2(int arr[][5], int size) {
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
 
+void display3(int *arr, int row, int col) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < col; j++) {
+			printf("%d ", *(arr + (col * i) + j));
+		}
+		printf("\n");
+	}
+}
--- test.c
+++ test.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-void display(int(*)[5],int);
+
+
+void display();
 int main(int argc, char **argv) {
-	int arr[5][5];
-	for (int i = 0; i < 5; i++) {
-		for(int j=0;j<5;j++){
-			arr[i][j]=5;
-		}
+	if(display==0){
+
 	}
-	display(arr,5);
-	return 0;
 }
 
-void display(int (*arr)[5],int size){
-	for (int i = 0; i < 5; i++) {
-			for(int j=0;j<5;j++){
-				printf("%d ",arr[i][j]);
-			}
-			printf("\n");
-		}
+void display(){
+
 }
 
 
